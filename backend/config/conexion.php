f276fbec57e5f99225d57f992a84df7f2d00c431
Jarenjuarez25 <juarezjarengamer@gmail.com>
2026-06-16 20:47:33 -0500

    conexion render

--- backend/config/conexion.php
+++ backend/config/conexion.php
@@ -1,14 +1,20 @@
 <?php
-$host = "localhost";
-$port = "5432";
-$dbname = "bd_personal_brigada";
-$user = "postgres";
-$password = "aurajuarez2019J";
 
-$conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
+$host = getenv('DB_HOST') ?: 'localhost';
+$port = getenv('DB_PORT') ?: '5432';
+$dbname = getenv('DB_NAME') ?: 'bd_personal_brigada';
+$user = getenv('DB_USER') ?: 'postgres';
+$password = getenv('DB_PASSWORD') ?: 'aurajuarez2019J';
+
+$conn = pg_connect("
+host=$host
+port=$port
+dbname=$dbname
+user=$user
+password=$password
+sslmode=require
+");
 
 if (!$conn) {
     die("Error de conexión");
 }
-
-?>
